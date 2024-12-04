d9896ae8897822e90ae75e00686a133948d2e908
Yuliia <yagoyulia@gmail.com>
2024-12-04 18:56:29 +0100

    final touch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Feed from './components/Feed';
 function App() {
   return (
     <div className="container mt-4">
-      <h1>Social Media Feed Reader</h1>
+      <h1>News Feed Reader</h1>
       <Feed />
     </div>
   );
